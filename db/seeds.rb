fa6eaf5daa04838fc92b500fd3b900ffae1f3ce8
mickael borrajo <mick.borrajo.ael@gmail.com>
2021-02-11 19:57:58 +0100

    folder seed.rb completed with instance

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating restaurants..."
+flat_a = {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 7,
+  number_of_guests: 8
+}
+flat_b = {
+  name: 'Garden Flat London',
+  address: ' London W9 1DT',
+  description: ' Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 750,
+  number_of_guests: 3
+}
+flat_c = {
+  name: 'Light ',
+  address: '10 ',
+  description: ' this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 5
+}
+
+[flat_a, flat_b, flat_c].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts "Finished!"
